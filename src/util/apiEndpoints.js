8fd75d321385f7ce1879366d7c4f9262c33b361d
uttamsingh3137 <uttampratp9871@gmail.com>
2025-08-10 18:13:59 +0530

    live frontend url

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8080/api/v1.0";
-export const BASE_URL = "https://money-manager-api-g7d1.onrender.com";
+export const BASE_URL = "https://money-manager-api-g7d1.onrender.com/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
 
 export const API_ENDPOINTS = {
