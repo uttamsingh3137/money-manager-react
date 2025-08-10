d0384e53f4d16eebcb32aace0470f197e2b28e53
uttamsingh3137 <uttampratp9871@gmail.com>
2025-08-10 18:41:10 +0530

    live frontend url

--- src/components/TransactionInfoCard.jsx
+++ src/components/TransactionInfoCard.jsx
@@ -31,7 +31,7 @@ const TransactionInfoCard = ({icon, title, date, amount, type, hideDeleteBtn, on
 
                     <div className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}>
                         <h6 className="text-xs font-medium">
-                            {type === 'income' ? '+': '-'} ${addThousandsSeparator(amount)}
+                            {type === 'income' ? '+': '-'} ₹{addThousandsSeparator(amount)}
                         </h6>
                         {type === 'income'? (
                             <TrendingUp size={15} />
